647df6715d3991de1d4c2574e346b8bffd2f70da
Krzysztof Grochocki <Krzysztof Grochocki@localhost>
2013-09-08 13:39:43 +0200

    * Przepisanie wtyczki pod kompilator Embarcadero C++ Builder 2010. * Poprawki kosmetyczne wygl?du formy. * Optymalizacja kodu. * Prawie ca?kowite rozdzielenie w kodzie obs?ugi User Tune z ustawianiem opisu. * Poprawa w notyfikacji UserTune. * Mo?liwo?? dodania os?b do listy wyj?tk?w, kt?re b?d? wy??czone z notyfikacji o aktualnie odtwarzanym przez nich utworu muzycznego. * Usuwanie z pobranego utworu ".mp3", ".wma", ".avi", ".mpg", ".mpeg", ".rmvb". * Mo?liwo?? wy??czenia zabezpieczenia przed pobieraniem aktualnie odtwarzanych film?w wideo. * Naprawa pobierania aktualnie odtwarzanego utworu w aTunes. * Poprawka w wycinaniu adres?w stron WWW z pobranego utworu.

--- MainFrm.h
+++ MainFrm.h
@@ -1,157 +1,156 @@
 //---------------------------------------------------------------------------
 #ifndef MainFrmH
 #define MainFrmH
+#include "cspin.h"
 #include <ActnList.hpp>
+#include <Buttons.hpp>
+#include <CheckLst.hpp>
 #include <Classes.hpp>
+#include <ComCtrls.hpp>
 #include <Controls.hpp>
 #include <ExtCtrls.hpp>
-#include <StdCtrls.hpp>
 #include <Menus.hpp>
-#include <ComCtrls.hpp>
-#include <CheckLst.hpp>
-#include "CSPIN.h"
-#include <IdBaseComponent.hpp>
-#include <IdCoder.hpp>
-#include <IdCoder3to4.hpp>
-#include <IdCoderMIME.hpp>
+#include <StdCtrls.hpp>
 //---------------------------------------------------------------------------
 class TMainForm : public TForm
 {
 __published:	// IDE-managed Components
-        TButton *SaveButton;
-        TActionList *ActionList;
-        TAction *aWinampDown;
-        TAction *aFoobarDown;
-        TAction *aMPCDown;
-        TTimer *Timer;
-        TAction *aLastFMDown;
-        TAction *aWMPDown;
-        TAction *aVUPlayerDown;
-        TAction *aXMPlayDown;
-        TAction *aCutSongNumber;
-        TAction *aReadSettings;
-        TAction *aSaveSettings;
-        TAction *aAutoDown;
-        TMemo *StatusMemo;
-        TAction *aSetStatusLooks;
-        TComboBox *TagsBox;
-        TLabel *TagRequestLabel;
-        TMemo *PreviewStatusMemo;
-        TLabel *SongTimerIntervalLabel1;
-        TTimer *SongTimer;
-        TLabel *SongTimerIntervalLabel2;
-        TLabel *TagInfoLabel1;
-        TAction *aiTunesDown;
-        TMemo *SongFromFile;
-        TAction *aALSongDown;
-        TLabel *TagInfoLabel2;
-        TAction *aPluginAQQRadioDown;
-        TAction *aSongbirdDown;
-        TBevel *Bevel3;
-        TLabel *AutoDownInfoLabel;
-        TButton *ResetButton;
-        TAction *aaTunesDown;
-        TLabel *AutoDownInfoLabel2;
-        TAction *aCutWWW;
-        TAction *aScreamerRadioDown;
-        TTimer *TurnOffTimer;
-        TBevel *Bevel1;
-        TBevel *Bevel2;
-        TAction *aResetSettings;
-        TPopupMenu *FoobarPopupMenu;
-        TMenuItem *FoobarDownload;
-        TPopupMenu *WMPPopupMenu;
-        TMenuItem *WMPDownload;
-        TPopupMenu *iTunesPopupMenu;
-        TMenuItem *iTunesDownload;
-        TPopupMenu *SongbirdPopupMenu;
-        TMenuItem *SongbirdDownload;
-        TLabel *UserTuneTimeNLabel;
-        TBevel *Bevel4;
-        TTimer *UserTuneTimer;
-        TTimer *UserTuneSongTimer;
-        TLabel *UserTuneLabel;
-        TLabel *UserTuneSLabel;
-        TBevel *Bevel5;
-        TLabel *UserTuneTimeSLabel1;
-        TLabel *UserTuneTimeSLabel2;
-        TAction *aCutWWWUserTune;
-        TTimer *AllowUserTuneNTimer;
-        TPageControl *PageControl;
-        TTabSheet *HandlingTabSheet;
-        TTabSheet *StatusLooksTabSheet;
-        TTabSheet *SettingsTabSheet;
-        TTabSheet *UserTuneTabSheet;
-        TCheckListBox *AutoDownCheckListBox;
-        TCheckListBox *AutoDownCheckListBoxPreview;
-        TCheckBox *RunPluginCheckBox;
-        TCheckBox *EnablePluginOnStartCheckBox;
-        TCheckBox *EnableFastOnOffCheckBox;
-        TCheckBox *SetOnlyInJabberCheckBox;
-        TCheckBox *BlockInvisibleCheckBox;
-        TCSpinEdit *SongTimerSpin;
-        TCheckBox *DisableSongTimerCheckBox;
-        TCheckBox *CutRadiostationNameCheckBox;
-        TCheckBox *CutWWWCheckBox;
-        TCheckBox *TimeTurnOffCheckBox;
-        TCSpinEdit *TimeTurnOffSpin;
-        TCheckBox *EnableUserTuneNCheckBox;
-        TCSpinEdit *UserTuneNSpin;
-        TCheckBox *EnableUserTuneSCheckBox;
-        TCSpinEdit *UserTuneSSpin;
-        TCheckBox *EnableAQQUserTuneSCheckBox;
-	TTimer *GetStatusTimer;
-        void __fastcall aWinampDownExecute(TObject *Sender);
-        void __fastcall aFoobarDownExecute(TObject *Sender);
-        void __fastcall aMPCDownExecute(TObject *Sender);
-        void __fastcall aLastFMDownExecute(TObject *Sender);
-        void __fastcall aWMPDownExecute(TObject *Sender);
-        void __fastcall TimerTimer(TObject *Sender);
-        void __fastcall SaveButtonClick(TObject *Sender);
-        void __fastcall FormShow(TObject *Sender);
-        void __fastcall aVUPlayerDownExecute(TObject *Sender);
-        void __fastcall aXMPlayDownExecute(TObject *Sender);
-        void __fastcall aCutSongNumberExecute(TObject *Sender);
-        void __fastcall aReadSettingsExecute(TObject *Sender);
-        void __fastcall aSaveSettingsExecute(TObject *Sender);
-        void __fastcall aAutoDownExecute(TObject *Sender);
-        void __fastcall aSetStatusLooksExecute(TObject *Sender);
-        void __fastcall TagsBoxSelect(TObject *Sender);
-        void __fastcall TagsBoxCloseUp(TObject *Sender);
-        void __fastcall TagsBoxDropDown(TObject *Sender);
-        void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
-        void __fastcall PreviewStatusMemoChange(TObject *Sender);
-        void __fastcall SongTimerTimer(TObject *Sender);
-        void __fastcall aiTunesDownExecute(TObject *Sender);
-        void __fastcall aALSongDownExecute(TObject *Sender);
-        void __fastcall aPluginAQQRadioDownExecute(TObject *Sender);
-        void __fastcall aSongbirdDownExecute(TObject *Sender);
-        void __fastcall SongbirdDownloadClick(TObject *Sender);
-        void __fastcall WMPDownloadClick(TObject *Sender);
-        void __fastcall iTunesDownloadClick(TObject *Sender);
-        void __fastcall FoobarDownloadClick(TObject *Sender);
-        void __fastcall aaTunesDownExecute(TObject *Sender);
-        void __fastcall AutoDownCheckListBoxPreviewClick(TObject *Sender);
-        void __fastcall AutoDownCheckListBoxPreviewDragDrop(
-          TObject *Sender, TObject *Source, int X, int Y);
-        void __fastcall AutoDownCheckListBoxPreviewDragOver(
-          TObject *Sender, TObject *Source, int X, int Y, TDragState State,
-          bool &Accept);
-        void __fastcall aCutWWWExecute(TObject *Sender);
-        void __fastcall aScreamerRadioDownExecute(TObject *Sender);
-        void __fastcall TurnOffTimerTimer(TObject *Sender);
-        void __fastcall aResetSettingsExecute(TObject *Sender);
-        void __fastcall AutoDownCheckListBoxPreviewMouseMove(
-          TObject *Sender, TShiftState Shift, int X, int Y);
-        void __fastcall UserTuneTimerTimer(TObject *Sender);
-        void __fastcall UserTuneSongTimerTimer(TObject *Sender);
-        void __fastcall aCutWWWUserTuneExecute(TObject *Sender);
-        void __fastcall AllowUserTuneNTimerTimer(TObject *Sender);
-        void __fastcall RunPluginCheckBoxClick(TObject *Sender);
-        void __fastcall TimeTurnOffCheckBoxClick(TObject *Sender);
-        void __fastcall EnableUserTuneNCheckBoxClick(TObject *Sender);
-        void __fastcall EnableUserTuneSCheckBoxClick(TObject *Sender);
-	void __fastcall GetStatusTimerTimer(TObject *Sender);
+		TButton *SaveButton;
+		TActionList *ActionList;
+		TAction *aWinampDown;
+		TAction *aFoobarDown;
+		TAction *aMPCDown;
+		TTimer *Timer;
+		TAction *aLastFMDown;
+		TAction *aWMPDown;
+		TAction *aVUPlayerDown;
+		TAction *aXMPlayDown;
+		TAction *aCutSongNumber;
+		TAction *aReadSettings;
+		TAction *aSaveSettings;
+		TAction *aAutoDown;
+		TMemo *StatusMemo;
+		TAction *aSetStatusLooks;
+		TComboBox *TagsBox;
+		TMemo *PreviewStatusMemo;
+		TLabel *SongTimerIntervalLabel1;
+		TTimer *SongTimer;
+		TLabel *SongTimerIntervalLabel2;
+		TAction *aiTunesDown;
+		TMemo *SongFromFile;
+		TAction *aALSongDown;
+		TAction *aPluginAQQRadioDown;
+		TAction *aSongbirdDown;
+		TLabel *AutoDownInfoLabel;
+		TButton *ResetButton;
+		TAction *aaTunesDown;
+		TLabel *AutoDownInfoLabel2;
+		TAction *aCutWWW;
+		TAction *aScreamerRadioDown;
+		TTimer *TurnOffTimer;
+		TBevel *Bevel3;
+		TBevel *Bevel2;
+		TAction *aResetSettings;
+		TPopupMenu *FoobarPopupMenu;
+		TMenuItem *FoobarDownload;
+		TPopupMenu *WMPPopupMenu;
+		TMenuItem *WMPDownload;
+		TPopupMenu *iTunesPopupMenu;
+		TMenuItem *iTunesDownload;
+		TPopupMenu *SongbirdPopupMenu;
+		TMenuItem *SongbirdDownload;
+		TLabel *UserTuneTimeNLabel;
+		TBevel *Bevel4;
+		TTimer *UserTuneTimer;
+		TTimer *UserTuneSongTimer;
+		TLabel *UserTuneLabel;
+		TLabel *UserTuneSLabel;
+		TBevel *Bevel5;
+		TLabel *UserTuneTimeSLabel1;
+		TLabel *UserTuneTimeSLabel2;
+		TAction *aCutWWWUserTune;
+		TTimer *AllowUserTuneNTimer;
+		TPageControl *PageControl;
+		TTabSheet *HandlingTabSheet;
+		TTabSheet *StatusTabSheet;
+		TTabSheet *SettingsTabSheet;
+		TTabSheet *UserTuneTabSheet;
+		TCheckListBox *AutoDownCheckListBox;
+		TCheckListBox *AutoDownCheckListBoxPreview;
+		TCheckBox *RunPluginCheckBox;
+		TCheckBox *EnablePluginOnStartCheckBox;
+		TCheckBox *EnableFastOnOffCheckBox;
+		TCheckBox *SetOnlyInJabberCheckBox;
+		TCheckBox *BlockInvisibleCheckBox;
+		TCSpinEdit *SongTimerSpin;
+		TCheckBox *DisableSongTimerCheckBox;
+		TCheckBox *CutRadiostationNameCheckBox;
+		TCheckBox *CutWWWCheckBox;
+		TCheckBox *TimeTurnOffCheckBox;
+		TCSpinEdit *TimeTurnOffSpin;
+		TCheckBox *EnableUserTuneNCheckBox;
+		TCSpinEdit *UserTuneNSpin;
+		TCheckBox *EnableUserTuneSCheckBox;
+		TCSpinEdit *UserTuneSSpin;
+		TCheckBox *EnableAQQUserTuneSCheckBox;
+		TTimer *GetStatusTimer;
+		TBevel *Bevel;
+		TAction *aExit;
+		TButton *UserTuneExceptionButton;
+		TAction *aAutoDownUserTune;
+		TBevel *Bevel1;
+		TCheckBox *MovieExceptionCheckBox;
+		void __fastcall aWinampDownExecute(TObject *Sender);
+		void __fastcall aFoobarDownExecute(TObject *Sender);
+		void __fastcall aMPCDownExecute(TObject *Sender);
+		void __fastcall aLastFMDownExecute(TObject *Sender);
+		void __fastcall aWMPDownExecute(TObject *Sender);
+		void __fastcall TimerTimer(TObject *Sender);
+		void __fastcall SaveButtonClick(TObject *Sender);
+		void __fastcall FormShow(TObject *Sender);
+		void __fastcall aVUPlayerDownExecute(TObject *Sender);
+		void __fastcall aXMPlayDownExecute(TObject *Sender);
+		void __fastcall aCutSongNumberExecute(TObject *Sender);
+		void __fastcall aReadSettingsExecute(TObject *Sender);
+		void __fastcall aSaveSettingsExecute(TObject *Sender);
+		void __fastcall aAutoDownExecute(TObject *Sender);
+		void __fastcall aSetStatusLooksExecute(TObject *Sender);
+		void __fastcall TagsBoxSelect(TObject *Sender);
+		void __fastcall TagsBoxCloseUp(TObject *Sender);
+		void __fastcall TagsBoxDropDown(TObject *Sender);
+		void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
+		void __fastcall PreviewStatusMemoChange(TObject *Sender);
+		void __fastcall SongTimerTimer(TObject *Sender);
+		void __fastcall aiTunesDownExecute(TObject *Sender);
+		void __fastcall aALSongDownExecute(TObject *Sender);
+		void __fastcall aPluginAQQRadioDownExecute(TObject *Sender);
+		void __fastcall aSongbirdDownExecute(TObject *Sender);
+		void __fastcall SongbirdDownloadClick(TObject *Sender);
+		void __fastcall WMPDownloadClick(TObject *Sender);
+		void __fastcall iTunesDownloadClick(TObject *Sender);
+		void __fastcall FoobarDownloadClick(TObject *Sender);
+		void __fastcall aaTunesDownExecute(TObject *Sender);
+		void __fastcall AutoDownCheckListBoxPreviewClick(TObject *Sender);
+		void __fastcall AutoDownCheckListBoxPreviewDragDrop(TObject *Sender, TObject *Source, int X, int Y);
+		void __fastcall AutoDownCheckListBoxPreviewDragOver(TObject *Sender, TObject *Source, int X, int Y, TDragState State, bool &Accept);
+		void __fastcall aCutWWWExecute(TObject *Sender);
+		void __fastcall aScreamerRadioDownExecute(TObject *Sender);
+		void __fastcall TurnOffTimerTimer(TObject *Sender);
+		void __fastcall aResetSettingsExecute(TObject *Sender);
+		void __fastcall AutoDownCheckListBoxPreviewMouseMove(TObject *Sender, TShiftState Shift, int X, int Y);
+		void __fastcall UserTuneTimerTimer(TObject *Sender);
+		void __fastcall UserTuneSongTimerTimer(TObject *Sender);
+		void __fastcall aCutWWWUserTuneExecute(TObject *Sender);
+		void __fastcall AllowUserTuneNTimerTimer(TObject *Sender);
+		void __fastcall RunPluginCheckBoxClick(TObject *Sender);
+		void __fastcall TimeTurnOffCheckBoxClick(TObject *Sender);
+		void __fastcall EnableUserTuneNCheckBoxClick(TObject *Sender);
+		void __fastcall EnableUserTuneSCheckBoxClick(TObject *Sender);
+		void __fastcall GetStatusTimerTimer(TObject *Sender);
+		void __fastcall aExitExecute(TObject *Sender);
+		void __fastcall UserTuneExceptionButtonClick(TObject *Sender);
+		void __fastcall aAutoDownUserTuneExecute(TObject *Sender);
+
 private:	// User declarations
 public:		// User declarations
         bool JustEnabled;
---
+++ UserTuneExceptionFrm.h
@@ -0,0 +1,43 @@
+//---------------------------------------------------------------------------
+
+#ifndef UserTuneExceptionFrmH
+#define UserTuneExceptionFrmH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ComCtrls.hpp>
+#include <ExtCtrls.hpp>
+#include <ActnList.hpp>
+//---------------------------------------------------------------------------
+class TUserTuneExceptionForm : public TForm
+{
+__published:	// IDE-managed Components
+	TBevel *Bevel;
+	TButton *SaveButton;
+	TTabControl *TabControl;
+	TListBox *JIDListBox;
+	TButton *AddButton;
+	TButton *DeleteButton;
+	TActionList *ActionList;
+	TAction *aExit;
+	TAction *aSaveSettings;
+	TAction *aReadSettings;
+	TLabel *Label1;
+	void __fastcall aExitExecute(TObject *Sender);
+	void __fastcall SaveButtonClick(TObject *Sender);
+	void __fastcall AddButtonClick(TObject *Sender);
+	void __fastcall JIDListBoxClick(TObject *Sender);
+	void __fastcall DeleteButtonClick(TObject *Sender);
+	void __fastcall aReadSettingsExecute(TObject *Sender);
+	void __fastcall aSaveSettingsExecute(TObject *Sender);
+
+private:	// User declarations
+public:		// User declarations
+	__fastcall TUserTuneExceptionForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TUserTuneExceptionForm *UserTuneExceptionForm;
+//---------------------------------------------------------------------------
+#endif
